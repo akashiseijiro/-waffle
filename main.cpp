d2a5ed82a7f4b06cc3e17dbb8f4a44ae22d649b6
akashiseijiro <49371103+akashiseijiro@users.noreply.github.com>
2019-04-11 13:00:59 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include "menu.h"
+
+int main()
+{
+	menu m;
+	m.display();             
+	m.menu_list();
+
+}
---
+++ menu.h
@@ -0,0 +1,14 @@
+#ifndef MENU_H
+#define MENU_H
+
+class menu
+{
+public:
+	menu();
+	void display();                     //to display menu
+	void menu_list();                   //to run switch 
+
+
+
+};
+#endif
