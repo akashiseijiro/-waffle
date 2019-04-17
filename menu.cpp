8f70c0fd301c619e4e4c36177aea73a6cf14635b
akashiseijiro <49371103+akashiseijiro@users.noreply.github.com>
2019-04-17 13:12:53 +0530

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -1,6 +1,7 @@
-﻿#include "menu.h"
+#include "menu.h"
 #include <iostream>
 #include<fstream>
+#include<windows.h>
 using namespace std;
 
 int ch;                                                                                             //for switch
@@ -25,64 +26,142 @@ void menu::display_menu()                                                       
 	cout << "***********************************************************************" << endl;
 
 }
-void order_manager::menu_list()                                                                            //implementiong switch case and loop
+void order_manager::get_menu()
 {
-	bool again = true;                                                                            // loop parameters
-	do
+	ofstream write;
+	write.open("order.text");
+	if (!write)
 	{
-		std::cout << "\tYour choice: ";
-		cin >> ch;
+		cout << "file can not open" << endl;
+		exit(-1);
+	}
 
+ma:
+mb:
+mc:
+	cout << "\n\n\tENTER YOUR ORDER (one order at a time): ";
+	cin >> ch;
+
+
+	cout << "\n\n\tDO YOU WANT TO UPDATE YOUR ORDER (y/n) :";
+
+	cin >> ye;
+
+	if (ye == "y" || ye == "Y")
+	{
+		goto mb;
+	}
+	else
+	{
 		switch (ch)
 		{
 		case 1:
-			cout << "you have selected Classic Waffle " << classic<<"\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
-			total  += classic;
-			again = true;
+			cout << "You have ordered Classic Waffle" << endl;
+
+
+			ma = 99;
+			mtotal = mtotal + ma;
+			write << "YOU ORDER  Classic Waffle : 99";
+			write << " \n";
+			write << "\n";
+			cout << "\n\n\n\tYOU ORDER  Classic Waffle " << endl << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
 			break;
 		case 2:
-			cout << "you have selected Nutella Waffle " << nutella << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
-			total += nutella;
-			again = true;
+			cout << "You have ordered Nutella Waffle" << endl;
+
+
+			mb = 109;
+			mtotal = mtotal + mb;
+			write << "YOU ORDER  Nutella Waffle : 109";
+			write << " \n";
+			write << "\n";
+			cout << "\n\n\n\tYOU ORDER  Nutella Waffle " << endl << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
 			break;
 		case 3:
-			cout << "you have Selected Fruit Waffle " << fruit << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
-			total += fruit;
-			again = true;
+			cout << "You have ordered Fruit Waffle" << endl;
+
+
+			mc = 109;
+			mtotal = mtotal + mc;
+			write << "YOU ORDER  Fruit Waffle : 109";
+			write << " \n";
+			write << "\n";
+			cout << "\n\n\n\tYOU ORDER  Fruit Waffle " << endl << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
 			break;
 		case 4:
-			cout << "you have selected Ferrero Rocher Waffle " << ferrero << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
-			total += ferrero;
-			again = true;
+			cout << "You have ordered Ferrero Rocher Waffle" << endl;
+
+
+			md = 129;
+			mtotal = mtotal + md;
+			write << "YOU ORDER  Ferrero Rocher Waffle : 129";
+			write << " \n";
+			write << "\n";
+			cout << "\n\n\n\tYOU ORDER  Ferrero Rocher Waffle " << endl << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
 			break;
 		case 5:
-			cout << "you have selected Nutella Pancake " << pancake << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
-			total += pancake;
-			again = true;
+			cout << "You have ordered Nutella Pancake" << endl;
+
+
+			me = 99;
+			mtotal = mtotal + me;
+			write << "YOU ORDER  Nutella Pancake : 99";
+			write << " \n";
+			write << "\n";
+			cout << "\n\n\n\tYOU ORDER  Nutella Pancake " << endl << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
 			break;
 		case 6:
-			cout << "you have selected Ice Cream Taco " << ice << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
-			total += ice;
-			again = true;
+			cout << "You have ordered Icecream Taco" << endl;
+
+
+			mf = 129;
+			mtotal = mtotal + mf;
+			write << "YOU ORDER  Icecream Taco : 129";
+			write << " \n";
+			write << "\n";
+			cout << "\n\n\n\tYOU ORDER  Icecream Taco " << endl << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
 			break;
 		case 7:
-			cout << "you have selected Walnut Brownie " << walnut << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
-			total += walnut;
-			again = true;
+			cout << "You have ordered Walnut Brownie" << endl;
+
+
+			mg = 79;
+			mtotal = mtotal + mg;
+			write << "YOU ORDER  Walnut Brownie : 79";
+			write << " \n";
+			write << "\n";
+			cout << "\n\n\n\tYOU ORDER  Walnut Brownie " << endl << "\tYOUR ORDER SUCCESSFULLY SAVED " << endl;
 			break;
 		case 8:
-			cout << "thank you for ordering....................." << " your bill is " << endl;
-			cout << total;
-			goto order;
+			cout << "\n\t\t\t\t\t thank you for ordering.........................";
+			exit(0);
+			break;
 		default:
-			cout << "Oops! Wrong menu choice." << endl;
-			cout << "enter proper value";
+			cout << "\n\n\t\t\t\t\t\t\tYOU ENTER WRONG ORDER KEY !!!!! " << endl;
+			goto mc;
 			break;
 		}
-	} while (again);                          
-}
-void order::bill()
-{
+
+	}
+
+	cout << "\n\tDO YOU WANT ANOTHER ORDER (yes/no) :";
+	cin >> y;
+	if (y == "yes" || y == "YES")
+	{
+		goto ma;
+	}
+	else
+	{
+		cout << "\n\t\t\t\t\t\t\tTHANK YOU FOR YOUR ORDER :) GENERATING BILL ";
+		for (int a = 1; a < 8; a++) // Change 'a<?' to how many * you want
+		{
+			Sleep(500);
+			cout << "...";
+		}
+		cout << "\n\t\t\t\t\t\t\t";
+		system("PAUSE");
+	}
+
 	system("cls");
 	cout << "\n\t\t\t\t\t\t\t ================================================" << endl;
 	cout << "\t\t\t\t\t\t\t|               Food Ordering System             |" << endl;
@@ -93,12 +172,19 @@ void order::bill()
 	cout << "\t\t\t\t\t\t\t  Bill No : CP51           Order : NB1353" << endl;
 	cout << endl;
 	cout << endl;
-	cout << "\t\t\t\t\t\t\t  Cashier :  shivi" << endl;
+	cout << "\t\t\t\t\t\t\t  Cashier :  Shivi" << endl;
 	cout << endl;
-	cout << "\t\t\t\t\t\t\t\t    YOUR TOTAL BILL IS Rs." << total << endl;
-
+	cout << "\t\t\t\t\t\t\t\t    YOUR TOTAL BILL IS Rs." << mtotal << endl;
+	write << "\t\t\t\t\t\t\t ";
+	write << "\t\t\t\t\t\t\tTOTAL BILL IS : " << mtotal;
 	cout << "\n\t\t\t\t\t\t\t ================================================" << endl;
 
 
-	
+	write.close();
 }
+
+order_manager::~order_manager()
+{
+}
+
+
