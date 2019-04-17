18357d6489d2e8a148d1d5464a2f505edb64e955
akashiseijiro <49371103+akashiseijiro@users.noreply.github.com>
2019-04-17 13:13:37 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "menu.h"
 #include"login.h"
-#include"item.h"
+
 using namespace std;
 
 int main()
@@ -10,28 +10,11 @@ int main()
 	login l;
 	l.login_details();
 	l.screen();
-	MenuInfo a;
-	a.input();
-	mClassicPancake b;
-	b.details();
-	mNutellaWaffle c;
-	c.details();
-	mFruitWaffle D;
-	D.details();
-	mFerrero h;
-	h.details();
-	mNutellaPancake n;
-	n.details();
-	mIce I;
-	I.details();
-	mWalnut W;
-	W.details();
 	menu m;
 	m.display_menu();             
 	order_manager o;
-	o.menu_list();
-	order B;
-	B.bill();
+	o.get_menu();
+	
 
 }
 
